ba33ad017d1d26868f18e50a71e1102cec222c75
chanmoni <122137115+chanmoni@users.noreply.github.com>
2023-05-27 16:54:54 +0900

    submit 3-1(2) 0527

--- LetsEngineer/Curriculum/3-1/index.php
+++ LetsEngineer/Curriculum/3-1/index.php
@@ -6,11 +6,11 @@ for ($i = 1; $i <= 100; $i++) {
         echo 'FizzBuzz!!';
     }
 
-    else if ($i % 3 === 0) {
+    else if ($i % 3 == 0) {
         echo 'Fizz!';
     }
 
-    else if ($i % 5 === 0) {
+    else if ($i % 5 == 0) {
         echo 'Buzz!';
     }
 
